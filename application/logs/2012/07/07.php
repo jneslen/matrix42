17760907a352d95cb41ba7ce7cc39fb1bb0eef9f
Jeff Neslen <jeff.neslen@matrix42.com>
2012-07-06 16:48:49 -0600

    merge and cleanup of submodules

---
+++ application/logs/2012/07/07.php
@@ -0,0 +1,8 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2012-07-07 00:47:29 --- ERROR: Kohana_Exception [ 0 ]: Attempted to load an invalid or missing module 'komponent' at 'MODPATH/komponent' ~ SYSPATH/classes/kohana/core.php [ 542 ]
+2012-07-07 00:47:29 --- STRACE: Kohana_Exception [ 0 ]: Attempted to load an invalid or missing module 'komponent' at 'MODPATH/komponent' ~ SYSPATH/classes/kohana/core.php [ 542 ]
+--
+#0 /Volumes/Files/Sites/darth/bane/application/bootstrap.php(153): Kohana_Core::modules(Array)
+#1 /Volumes/Files/Sites/darth/bane/index.php(96): require('/Volumes/Files/...')
+#2 {main}
