fdba53d7aa927906b4c6a7f5471cdc0261beb292
jneslen <jeff.neslen@matrix42.com>
2012-07-30 11:01:30 -0600

    updated german 500 error message to more appropriate message.

--- application/views/de/error/500.php
+++ application/views/de/error/500.php
@@ -1,6 +1,6 @@
 <div class="padded">
 	<div style="margin-top:50px;width:960px;margin:0 auto">
-		<h1>Mist! Verdammt! Es gab einen Fehler mit der Website.</h1>
+		<h1>Entschuldigen Sie! Es wurde ein unerwarteter Fehler auf der Website entdeckt.</h1>
 		<p>Der Webmaster wurde benachrichtigt.</p>
 	</div>
 </div>
