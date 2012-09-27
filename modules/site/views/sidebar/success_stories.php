5ee655a705bb80d71ec52356dc28ad6d76821a97
jneslen <jeff.neslen@matrix42.com>
2012-09-27 08:50:23 -0600

    corrected gramar on success story quote

--- modules/site/views/sidebar/success_stories.php
+++ modules/site/views/sidebar/success_stories.php
@@ -1,5 +1,5 @@
 <div class="well margin-right">
-	<p class="quote"><span class="italics">&quot;Matrix42’s most noteworthy capability is management of citrix xenapp and xendesktop hybrid physical and virtual desktop infrastructure.  It’s the only citrix management solution in the wave&quot;</span></p>
+	<p class="quote"><span class="italics">&quot;Matrix42’s most noteworthy capability is management of Citrix XenApp and XenDesktop hybrid physical and virtual desktop infrastructure.  It’s the only Citrix management solution in the wave&quot;</span></p>
 	<img src="/assets/img/partners/forrester-logo.png" alt="Forrester" class="right" />
 	<div class="clear"></div>
 	<p class="quote"><span class="italics">&quot;Matrix42 enables organizations to manage physical, mobile and virtual environments with a single product. It is the only vendor in the client management market with this capability.&quot;</span></p>
