16cedd462a1af7f0ff8e8935aaea9ec7f7f5a949
jneslen <jeff.neslen@matrix42.com>
2012-10-14 11:45:17 -0600

    replace footer for email

--- application/views/email/_footer.php
+++ application/views/email/_footer.php
@@ -1,40 +1,40 @@
-					</td>
-				</tr>
-				<tr>
-					<td style="border-style: none; border-width: 0px; white-space:nowrap;" nowrap="nowrap" align="left" valign="top" bgcolor="#FFFFFF">
-						<a href="http://www.facebook.com/matrix42page">
-							<img alt="facebook" style="display:block;float:left;" src="http://www.matrix42.com/assets/img/email/facebook-quiet.png" border="0" height="31" width="41" />
-						</a>
-						<a href="http://blog.matrix42.com">
-							<img alt="blog" style="display:block;float:left;" src="http://www.matrix42.com/assets/img/email/blog-quiet.png" border="0" height="31" width="41" />
-						</a>
-						<a href="https://twitter.com/matrix42man">
-							<img alt="twitter" style="display:block;float:left;" src="http://www.matrix42.com/assets/img/email/twitter-quiet.png" border="0" height="31" width="41" />
-						</a>
-						<div style="clear:both; margin:0; padding:0;"></div><br />
-						<img style="display:block;" src="http://www.matrix42.com/assets/img/blank.gif" border="0" height="5" width="124" />
-					</td>
-				</tr>
-				<tr>
-					<td style="border-style: none; border-width: 0px;background:#fff;height:24px;" align="left" valign="bottom">
-					</td>
-				</tr>
-				</tbody>
-			</table>
-			<table style="border-width: 0px; padding: 10px;" border="0" cellpadding="0" cellspacing="0" width="800">
-				<tbody>
-				<tr>
-					<td class="options" align="center">
-						<span class="white">
+</td>
+</tr>
+<tr>
+	<td style="border-style: none; border-width: 0px; white-space:nowrap;" nowrap="nowrap" align="left" valign="top" bgcolor="#FFFFFF">
+		<a href="http://www.facebook.com/matrix42page">
+			<img alt="facebook" style="display:block;float:left;" src="http://www.matrix42.com/assets/img/email/facebook-quiet.png" border="0" height="31" width="41" />
+		</a>
+		<a href="http://blog.matrix42.com">
+			<img alt="blog" style="display:block;float:left;" src="http://www.matrix42.com/assets/img/email/blog-quiet.png" border="0" height="31" width="41" />
+		</a>
+		<a href="https://twitter.com/matrix42man">
+			<img alt="twitter" style="display:block;float:left;" src="http://www.matrix42.com/assets/img/email/twitter-quiet.png" border="0" height="31" width="41" />
+		</a>
+		<div style="clear:both; margin:0; padding:0;"></div><br />
+		<img style="display:block;" src="http://www.matrix42.com/assets/img/blank.gif" border="0" height="5" width="124" />
+	</td>
+</tr>
+<tr>
+	<td style="border-style: none; border-width: 0px;background:#fff;height:24px;" align="left" valign="bottom">
+	</td>
+</tr>
+</tbody>
+</table>
+<table style="border-width: 0px; padding: 10px;" border="0" cellpadding="0" cellspacing="0" width="800">
+	<tbody>
+	<tr>
+		<td class="options" align="center">
+								<span class="white">
 
-						</span>
-					</td>
-				</tr>
-				</tbody>
-			</table>
+								</span>
 		</td>
 	</tr>
 	</tbody>
+</table>
+</td>
+</tr>
+</tbody>
 </table>
 </body>
 </html>
